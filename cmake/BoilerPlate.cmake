3584a404b5b7669c558dd6a8e81ac162ac0553e9
Edward Rudd <urkle@outoforder.cc>
2015-01-27 13:35:26 -0500

    indentation fixes
    - also remove outdated comment about cmake 2.8.11

--- cmake/BoilerPlate.cmake
+++ cmake/BoilerPlate.cmake
@@ -70,20 +70,15 @@ elseif(LINUX)
             set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} -msse4")
         endif()
     endif()
-    
+
     set_property(GLOBAL PROPERTY LIBRARY_RPATH_DIRECTORY ${LIB_RPATH_DIR})
-    
+
     set(CMAKE_SKIP_BUILD_RPATH              TRUE)
     set(CMAKE_BUILD_WITH_INSTALL_RPATH      TRUE)
     set(CMAKE_INSTALL_RPATH                 "\$ORIGIN/${LIB_RPATH_DIR}")
     set(CMAKE_INSTALL_RPATH_USE_LINK_PATH   FALSE)
 elseif(APPLE)
     set(PLATFORM_PREFIX             "macosx")
-
-    ## NOTE setting the rpath this way only works with CMAKE 2.8.12+
-    # A workaround for 2.8.11 is to also set 
-    # set(CMAKE_XCODE_ATTRIBUTE_LD_RUNPATH_SEARCH_PATHS ${BIN_RPATH})
-    ##
 
     set(BIN_RPATH "@executable_path/../Frameworks")
 
